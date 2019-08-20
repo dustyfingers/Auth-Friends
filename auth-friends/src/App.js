145f86eae550ecc261a851ff2a730188fae947bb
dustyfingers <dustyfingers01257@gmail.com>
2019-08-20 15:28:31 -0600

    built login and nav components

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Login from './components/Login/Login.component'
 import Friends from './components/Friends/Friends.component'
+import Navbar from './components/Navbar/Navbar.component'
 
 const App = () => {
   return (
     <div className="App">
-      AUTH "FRIENDS"
-      <Login />
-      <Friends />
+      <Navbar />
+      <Switch>
+        <Route path='/api/login' component={Login} />
+        <Route path='/api/friends' component={Friends} />
+      </Switch>
     </div>
   );
 }
--- auth-friends/src/components/Login/Login.component.js
+++ auth-friends/src/components/Login/Login.component.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 import './Login.styles.sass'
 
 const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = evt => {
+        setCredentials({ ...credentials, [evt.target.name]: evt.target.value })
+    };
+
+    const login = async evt => {
+        evt.preventDefault();
+        try {
+            let { data } = await axios.post('http://localhost:5000/api/login', credentials);
+            localStorage.setItem('authToken', data.payload);
+        } catch (err) {
+            console.error(err.message);
+        }
+
+    }
+
     return (
         <div>
             LOGIN COMPONENT
+            <form onSubmit={login}>
+                Username:
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    onChange={evt => handleChange(evt)} />
+                Password:
+                <input
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={evt => handleChange(evt)} />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
